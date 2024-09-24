7ba73503c4303d3f319f4248ab5fef2c6531d299
jp-bz <jbedoyaz@unal.edu.co>
2024-09-24 12:36:44 -0500

    commit

---
+++ Seguimiento 6/Friends2/src/friends2/Friends2.java
@@ -0,0 +1,12 @@
+
+package friends2;
+
+public class Friends2 {
+
+    public static void main(String[] args) {
+        MainForm frm = new MainForm();
+        frm.setVisible(true);
+        
+    }
+    
+}
